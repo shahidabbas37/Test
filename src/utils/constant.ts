a265c988e94a7510e41bd55f6a1fef717530103e
Sayed Shahid Abbas <92936954+shahidabbas37@users.noreply.github.com>
2022-01-03 19:32:02 +0500

    remove some information

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,7 +1,6 @@
 export const MongoCluster ="shahid";
 export const MongoDbName ="Test";
-export const Mongo_user_name="shahid";
-export const Mongo_Pass ="shahid0300";
-export const SECRIT_KEY ="%Shahid*36@%";
+export const Mongo_user_name="";
+export const Mongo_Pass ="";
+export const SECRIT_KEY =""
 
-//mongodb+srv://shahid:<password>@shahid.coszq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
